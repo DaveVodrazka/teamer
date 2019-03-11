f8b0decd05a5a94ac7b1ff6e5a99e13f41db71b8
DaveVodrazka <dave.vodrazka@gmail.com>
2019-03-11 23:10:28 +0100

    feat: Add team distribution, only console log

--- src/components/ControlePanel.js
+++ src/components/ControlePanel.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import shuffle from '../helpers/shuffle';
 
 class ControlPanel extends Component {
     constructor(props) {
         super(props);
         this.state = props.active;
     }
+
+    pickTeams(people) {
+        const res = shuffle(people);
+        console.log(`Blue team is ${res[0]} and ${res[1]}.\nRed team is ${res[2]} and ${res[3]}.`);
+    }
     
     render() {
         return (
             <div className="control-panel-container">
-                <button className={'pickbutton ' + (this.state.active.length === 4 ? 'pickbutton-active' : '')}>Team It!</button>
+                <button
+                    className={'pickbutton ' + (this.state.active.length === 4 ? 'pickbutton-active' : '')}
+                    onClick={() => this.pickTeams(this.state.active)}
+                >
+                    Team It!
+                </button>
             </div>
         );
     }
