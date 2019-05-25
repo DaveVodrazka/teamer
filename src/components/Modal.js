b1275d6c3de95245d4dd8eb97ae18939b27eb7b2
DaveVodrazka <dave.vodrazka@gmail.com>
2019-05-25 22:11:25 +0200

    feature: send results

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PlayerPortrait from './PlayerPortrait';
-
 import closeIcon from '../assets/close.svg';
 
-function result(results) {
-    const blueTeam = {
-        playerOne: results[0],
-        playerTwo: results[1],
+function sendScore(score, players, done) {
+    if (score.red === 0 && score.blue === 0) {
+        // No score input
+        return null;
     }
-    const redTeam = {
-        playerOne: results[2],
-        playerTwo: results[3],
+    const result = {
+        bluePlayers: [players[0], players[1]],
+        redPlayers: [players[2], players[3]],
+        blueScore: score.blue,
+        redScore: score.red
     }
-    console.log(blueTeam);
-    console.log(redTeam);
+    if (score.blue > score.red) {
+        result.winner = 'blue';
+    } else if (score.blue < score.red) {
+        result.winner = 'red';
+    } else if (score.blue === score.red) {
+        result.winner = 'draw';
+    }
+    score.blue = '';
+    score.red = '';
+
+    // TODO: send data logic
+    console.log(result);
+    done();
 }
 
 function Modal(props) {
+    const [showRes, changeVis] = useState(false);
+    const [score, changeScore] = useState({blue: '', red: ''});
+
     if(props.people && props.people.length > 3) {
         return(
             <div className={'modal ' + (props.show ? 'show' : 'hide')}>
@@ -24,11 +39,15 @@ function Modal(props) {
                     <button className="close-button" onClick={props.close}>
                         <img src={closeIcon} alt="close button" />
                     </button>
-                    <button className="result-button" onClick={() => result(props.people)}>
+                    <button className="result-button" onClick={() => {
+                        if (showRes) {
+                            sendScore(score, props.people, props.close);
+                        }
+                        changeVis(!showRes);
+                    }}>
                         Result
                     </button>
                 </div>
- 
 
                 <div className="team-holder blue">
                     <PlayerPortrait person={props.people[0]} />
@@ -37,6 +56,15 @@ function Modal(props) {
                 <div className="team-holder red">
                     <PlayerPortrait person={props.people[2]} />
                     <PlayerPortrait person={props.people[3]} />
+                </div>
+
+                <div className={'result ' + (showRes ? 'show' : 'hide')}>
+                    <div className='left-one'>
+                        <input className='score-input' type="number" value={score.blue} onChange={(event) => changeScore({blue: parseInt(event.target.value), red: score.red})} />
+                    </div>
+                    <div className='right-one'>
+                        <input className='score-input' type="number" value={score.red} onChange={(event) => changeScore({blue: score.blue, red: parseInt(event.target.value)})} />
+                    </div>
                 </div>
             </div>
         );
